2452d1ecb08ceeac88fdfc02d2fa070c34fc3caf
Juan Pablo Lopez <tu@ejemplo.com>
2025-05-24 17:43:38 -0400

    ed
    dc

--- app.py
+++ app.py
@@ -8,7 +8,7 @@ app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///ferremas.db'  # Usa SQLite o 
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 db = SQLAlchemy(app)
 
-# Modelo de Producto
+# Modelo de Producto x
 class Producto(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     name = db.Column(db.String(100), nullable=False)
